c43c9ebabdd5232a0f4c0fefa77d7e96c28cf232
Phuoc Nguyen <ba.phuoc.nbp@gmail.com>
2016-05-02 22:06:43 +0700

    make it possible to use ng-disabled and ng-change

--- src/bc-phone-number.js
+++ src/bc-phone-number.js
@@ -37,12 +37,17 @@ angular.module('bcPhoneNumber', ['bcPhoneNumberTemplates', 'ui.bootstrap'])
       defaultCountryCode: '@defaultCountry',
       selectedCountry: '=',
       isValid: '=',
-      ngModel: '='
+      ngModel: '=',
+      ngChange: '=',
+      ngDisabled: '='
     },
     link: function(scope, element, attrs, ctrl) {
       scope.selectedCountry = bcCountries.getCountryByIso2Code(scope.defaultCountryCode || 'us');
       scope.allCountries = bcCountries.getAllCountries();
       scope.number = scope.ngModel;
+      scope.changed = function() {
+        scope.ngChange();
+      }
 
       if (scope.preferredCountriesCodes) {
         var preferredCodes = scope.preferredCountriesCodes.split(' ');
