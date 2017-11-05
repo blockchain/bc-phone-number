e29bc8f55659b0f814ad091712191011fd992dca
David Cain <davidjosephcain@gmail.com>
2017-11-05 10:06:57 -0800

    Fix throw syntax
    `new` should be used on constructors.

--- src/bc-phone-number.js
+++ src/bc-phone-number.js
@@ -15,7 +15,7 @@ angular.module('bcPhoneNumber', ['bcPhoneNumberTemplates', 'ui.bootstrap'])
 .directive('bcPhoneNumber', function() {
 
   if (typeof (bcCountries) === 'undefined') {
-    throw new('bc-countries not found, did you forget to load the Javascript?');
+    throw 'bc-countries not found, did you forget to load the Javascript?';
   }
 
   function getPreferredCountries(preferredCodes) {
