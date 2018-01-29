6d9c6c94cea1c36971749818702e3a560c368ad3
plondon <philip@blockchain.com>
2018-01-29 18:03:20 -0500

    Remote jQuery from dep list

--- dist/js/bc-phone-number.js
+++ dist/js/bc-phone-number.js
@@ -53,7 +53,7 @@ angular.module('bcPhoneNumber', ['bcPhoneNumberTemplates', 'ui.bootstrap'])
       scope.number = scope.ngModel;
       scope.changed = function() {
         scope.ngChange();
-      }
+      };
 
       if (scope.preferredCountriesCodes) {
         var preferredCodes = scope.preferredCountriesCodes.split(' ');
--- src/bc-phone-number.js
+++ src/bc-phone-number.js
@@ -49,7 +49,7 @@ angular.module('bcPhoneNumber', ['bcPhoneNumberTemplates', 'ui.bootstrap'])
       scope.number = scope.ngModel;
       scope.changed = function() {
         scope.ngChange();
-      }
+      };
 
       if (scope.preferredCountriesCodes) {
         var preferredCodes = scope.preferredCountriesCodes.split(' ');
